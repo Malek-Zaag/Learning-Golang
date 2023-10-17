50fb4fe163708000218bef46b7049ffd0e9ae6e3
Malek <zaag.malek1@gmail.com>
2023-10-17 11:15:47 +0100

    jwt_impl_modif

--- API/api.go
+++ API/api.go
@@ -6,8 +6,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"users/token"
+	"os"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
@@ -18,7 +19,7 @@ func (s *APIServer) Run() {
 	router := mux.NewRouter()
 	router.HandleFunc("/create", s.handleCreateUser)
 	router.HandleFunc("/users", s.handleGetUsers)
-	router.HandleFunc("/user", token.ValidateJWT(s.handleGetUserByEmail))
+	router.HandleFunc("/user", JWTMiddleware(s.handleGetUserByEmail))
 	log.Println("JSON API server running on port:", s.listenAddr[1:])
 	http.ListenAndServe(s.listenAddr, router)
 }
@@ -71,4 +72,42 @@ func WriteRequest(w http.ResponseWriter, status int, v any) error {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(v)
+}
+
+func JWTMiddleware(next func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, err := validateJWT("random string")
+		log.Panic(err)
+		next(w, r)
+	}
+}
+
+func createJWT(user *User) (string, error) {
+	claims := &jwt.MapClaims{
+		"expiresAt": 15000,
+		"userEmail": user.Email,
+	}
+
+	secret := os.Getenv("JWT_SECRET")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(secret))
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	secret := os.Getenv("JWT_SECRET")
+
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(secret), nil
+	})
+}
+
+func permissionDenied(w http.ResponseWriter) {
+	WriteRequest(w, http.StatusUnauthorized, "permission denied")
 }
